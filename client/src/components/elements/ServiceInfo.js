88ac3766733a6f197aa4b22f7099fe4def6ebe7c
Helene-mb <65496401+Helene-mb@users.noreply.github.com>
2021-04-12 23:17:37 +0200

    Design service info add margin bottom

--- client/src/components/elements/ServiceInfo.js
+++ client/src/components/elements/ServiceInfo.js
@@ -28,13 +28,13 @@ export default class ServiceInfo extends React.Component {
         let address0 = "0x0000000000000000000000000000000000000000";
 
         return (
-            <div>
-                <p>Nom du service : <span className="service-info-p">{myService.description}</span></p>
-                <p>Type de mesure : <span className="service-info-p">{myService.measureType}</span></p>
-                <p>Fréquence des mesures : <span className="service-info-p">Toute les {myService.nbTime} {myService.timeCode}</span></p>
-                <p>Adresse du bridge : <span className="service-info-p">{myService.bridgeAddress===address0?"En attente":myService.bridgeAddress}</span></p>
-                <p>Adresse du législateur : <span className="service-info-p">{myService.legislatorAddress===address0?"En attente":myService.legislatorAddress}</span></p>
-                <p>Adresse du technicien : <span className="service-info-p">{myService.techMasterAddress===address0?"En attente":myService.techMasterAddress}</span></p>
+            <div className="service-info">
+                <p>Nom du service : <span className="service-info-details">{myService.description}</span></p>
+                <p>Type de mesure : <span className="service-info-details">{myService.measureType}</span></p>
+                <p>Fréquence des mesures : <span className="service-info-details">Toute les {myService.nbTime} {myService.timeCode}</span></p>
+                <p>Adresse du bridge : <span className="service-info-details">{myService.bridgeAddress===address0?"En attente":myService.bridgeAddress}</span></p>
+                <p>Adresse du législateur : <span className="service-info-details">{myService.legislatorAddress===address0?"En attente":myService.legislatorAddress}</span></p>
+                <p>Adresse du technicien : <span className="service-info-details">{myService.techMasterAddress===address0?"En attente":myService.techMasterAddress}</span></p>
 
             </div>
         );      

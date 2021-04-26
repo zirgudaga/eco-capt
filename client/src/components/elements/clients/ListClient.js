6f78dd7bab8ce97749e01a3ea041b25c935dd472
Helene-mb <65496401+Helene-85@users.noreply.github.com>
2021-04-26 14:28:47 +0200

    Design list customers

--- client/src/components/elements/clients/ListClient.js
+++ client/src/components/elements/clients/ListClient.js
@@ -6,21 +6,21 @@ export default class ListClient extends React.Component {
 
     render() {
         return (
-            <div className="list-client-body">
+            <div className="list-body">
                 <div className="list-ul">
                     {this.props.state.listElements.length > 0 
                         ?
                         this.props.state.listElements.map((element, index) => (
-                            <div className="list-client" key={"elementKey"+index} onClick={ () => this.props.setElementFocus(index) }>
-                                <span>{element.description}</span> 
-                            </div>  
+                            <button className="list-button" key={"elementKey"+index} onClick={ () => this.props.setElementFocus(index) }>
+                                {element.description} 
+                            </button>  
                         ))
                         :
                         " Aucun client"
                     }
                 </div>
                 {(this.props.state.myTypeUser==='1') &&
-                    <button type="button" className="list-client-cta" 
+                    <button type="button" className="list-cta" 
                         onClick= { () => this.props.addElement() }>New customer
                     </button> 
                 }
